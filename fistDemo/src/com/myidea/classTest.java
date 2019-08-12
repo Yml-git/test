3827a7e4ee28c5e00ab73425fa5d78f7fdfb79f1
余明亮 <33717114+Bright-I@users.noreply.github.com>
2019-08-12 17:27:39 +0800

    git test

---
+++ fistDemo/src/com/myidea/classTest.java
@@ -0,0 +1,14 @@
+package com.myidea;
+
+public class classTest {
+    public static void  main(String [] args){
+        int i = 10;
+        String str = "*";
+        for (int j = 0;j<i;j++){
+            str = str + "*";
+            System.out.println(str);
+        }
+        System.out.println("Hello Word!"+i);
+
+    }
+}
